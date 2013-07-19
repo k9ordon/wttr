1091d303893debada59b52866341525130f0eed7
k9ordon <k9ordon@gmail.com>
2013-07-19 22:19:10 +0200

    location less/view

--- frontend/view/location-index.php
+++ frontend/view/location-index.php
@@ -1,8 +1,14 @@
 <? include "_head.php" ?>
 
+<div id="locationWrap">
 <h1>Wetter <?=$this->location['name']?></h1>
 
-<hr><h2>Today</h2>
+<div id="today">
+	<h2><?=$this->location['today']['temp'] ?>°C <?=$this->location['today']['type']?></h2>
+</div>
+
+
+<div style="display:none">
 <pre>
 	<? var_dump($this->location['today']); ?>
 </pre>
@@ -11,6 +17,7 @@
 <pre>
 	<? var_dump($this->location['forecast'][0]); ?>
 </pre>
+</div>
 
 
 <? include "_foot.php" ?>
