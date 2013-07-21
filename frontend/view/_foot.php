78dbade9f09e0dfe78d30720eef9e51d78949663
k9ordon <k9ordon@gmail.com>
2013-07-21 21:29:29 +0200

    embed charts.js

--- frontend/view/_foot.php
+++ frontend/view/_foot.php
@@ -1,9 +1,8 @@
 </div>
-<? /*
+
 <? foreach(array("assets/vendor/raphy-charts/vendor/raphael.min.js", "assets/vendor/raphy-charts/compiled/charts.js") as $s) : ?>
 	<script type="text/javascript" src="<?=$s?>"></script>
 <? endforeach; ?>
-*/ ?>
 
 <script>
 	<? foreach(array("assets/js/locationcard.js", "assets/js/app.js") as $s) : ?>
