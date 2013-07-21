3f6a4873b3cbf6d4090d50c9c6dc85edb95ab43d
k9ordon <k9ordon@gmail.com>
2013-07-21 13:38:45 +0200

    uniform time key, better fake days

--- fakeData/locationdata.php
+++ fakeData/locationdata.php
@@ -1,30 +1,39 @@
 <? 
 
-function fakeDay($addDays) {
+function fakeDay($addDays = 0, $timestamp = false) {
 	global $config;
 
-	$ts = strtotime(sprintf('+ %s days 0:00', $addDays));
+	$ts = $timestamp ? $timestamp : strtotime(sprintf('+ %s days 0:00', $addDays));
 	return array(
-		'day' => $ts,
+		'time' => $ts,
 		'type' => $config['weatherTypes'][array_rand($config['weatherTypes'])],
+		'icon' => $icons[array_rand($icons)],
 		'temp' => rand(10,30),
 		'min' => rand(10,20),
 		'max' => rand(20,30),
 		'rain' => rand(0,20),
-		'detail' => fakeDayHours($ts)
+		'detail' => fakeDayHours($ts, 3, 7)
 	);
 }
 
-function fakeDayHours($dayTimestamp) {
+function fakeDayHours($start = 0, $intervall = 3, $limit = 7) {
 	$icons = array('icon-sun', 'icon-moon', 'icon-cloudy', 'icon-cloud', 'icon-rainy', 'icon-lightning');
 	$item = array();
-	for($h = 0; $h <= 23; $h=$h+3) {
+
+	for($h = (int)date('h', $start); 
+		$h < (date('h', $start) + ($intervall * $limit)); 
+		$h = $h+$intervall) {
+		//var_dump([$h, $start, $intervall, $limit, 'max' => (date('h', $start) + ($intervall * $limit))]);continue;
+
 		$item[] = array(
 			'icon' => $icons[array_rand($icons)],
-			'hour' => $dayTimestamp + ($h * 60 * 60),
+			'type' => $config['weatherTypes'][array_rand($config['weatherTypes'])],
+			'time' => $start + ($h * 60 * 60),
 			'temp' => rand(10, 30),
 			'rain' => rand(0,20)
 		);
+
+		if($h > 100) exit;
 	}
 	return $item;
 }
@@ -33,6 +42,6 @@ $config['fake']['locationData'] = array(
 	'name' => 'Linz',
 	'today' => fakeDay(0),
 	'forecast' => array(
-		fakeDay(1), fakeDay(2), fakeDay(3), fakeDay(4), fakeDay(5), fakeDay(6), fakeDay(7)
+		fakeDay(0), fakeDay(1), fakeDay(2), fakeDay(3), fakeDay(4), fakeDay(5), fakeDay(6)
 	)
 );
