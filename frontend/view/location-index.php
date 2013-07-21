e5803a24ceb534252fb3524074c3500aa81597da
k9ordon <k9ordon@gmail.com>
2013-07-21 13:40:02 +0200

    added forcast

--- frontend/view/location-index.php
+++ frontend/view/location-index.php
@@ -1,40 +1,46 @@
 <? include "_head.php" ?>
 
-<div id="locationWrap" class="type<?=ucfirst($this->location['today']['type']['class'])?>">
+<div id="locationWrap">
 	<h1>Wetter <?=$this->location['name']?></h1>
 
-	<div id="today">
+	<div class="dayCard">
 		<h2><?=$this->location['today']['temp'] ?>°C</h2>
 		<h3><?=$this->location['today']['type']['name']?></h3>
-		<?/*<h4><?=date('d.m.Y', $this->location['today']['day'])?></h4>*/?>
+		<?/*<h4><?=date('d.m.Y', $this->location['today']['time'])?></h4>*/?>
+	
+		<canvas id="dayChart" class="dayChart" width="640" height="400"></canvas>
+
+		<div class="detail">
+			<? foreach($this->location['today']['detail'] as $detail) : ?>
+			<div class="hour">
+				<p class="icon"><span class="<?=$detail['icon']?>"></span></p>
+				<p class="time"><?=date('H:i', $detail['time'])?></p>
+				<p class="temp"><?=$detail['temp']?>°C</p>
+				<p class="rain"><?=$detail['rain']?>%</p>
+			</div>
+			<? endforeach; ?>
+		</div>
 	</div>
 
-	<div class="detail">
-		<? foreach($this->location['today']['detail'] as $detail) : ?>
-		<div class="hour">
+	<div id="forecast">
+		<? foreach($this->location['forecast'] as $idx => $day) : ?>
+		<div class="day<?=$idx==0?' active':''?>">
 			<p class="icon"><span class="<?=$detail['icon']?>"></span></p>
-			<p class="time"><?=date('H:i', $detail['hour'])?></p>
+			<p class="time">
+				<?=$this->config['weekdaysShort'][date('w', $day['time'])]?>
+				<?=date('d.m', $day['time'])?>
+			</p>
 			<p class="temp"><?=$detail['temp']?>°C</p>
 			<p class="rain"><?=$detail['rain']?>%</p>
 		</div>
 		<? endforeach; ?>
 	</div>
-
-	<div style="display:none">
-	<pre>
-		<? var_dump($this->location['today']); ?>
-	</pre>
-
-	<hr><h2>Forecast</h2>
-	<pre>
-		<? var_dump($this->location['forecast'][0]); ?>
-	</pre>
-	</div>
 </div>
 
-<div id="locationBackground">
+<div id="locationBackground" class="type<?=ucfirst($this->location['today']['type']['class'])?>">
+	<? /*
 	<img src="http://farm9.staticflickr.com/8278/8710831494_63c5940287_o.jpg" alt="Laufrunde durch Linz">
-	<a href="http://www.flickr.com/photos/friesenecker/8710831494/" title="Laufrunde durch Linz von Daniel Friesenecker bei Flickr">c by</a>
+	<a href="http://www.flickr.com/photos/friesenecker/8710831494/" title="Laufrunde durch Linz von Daniel Friesenecker bei Flickr">c by</a> */?>
 </div>
 
 <? include "_foot.php" ?>
