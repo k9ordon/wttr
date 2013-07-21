d89c058e57246ac21ca83510fc23cbacab98856b
k9ordon <k9ordon@gmail.com>
2013-07-21 21:28:35 +0200

    styling for local

--- frontend/controller/location.php
+++ frontend/controller/location.php
@@ -3,6 +3,7 @@
 class Controller_Location extends Controller {
 
 	public function Action_Index() {
+		$this->htmlClassList[] = 'local';
 		$this->getLocationData();
 		$this->render('location-index');
 	}
