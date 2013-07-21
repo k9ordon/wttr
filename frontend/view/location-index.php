f393672bd1cebd503f5045957d7c3bb1410c642b
k9ordon <k9ordon@gmail.com>
2013-07-21 14:25:03 +0200

    forcast restyle

--- frontend/view/location-index.php
+++ frontend/view/location-index.php
@@ -6,7 +6,7 @@
 	<div class="dayCard">
 		<h2><?=$this->location['today']['temp'] ?>°C</h2>
 		<h3><?=$this->location['today']['type']['name']?></h3>
-		<?/*<h4><?=date('d.m.Y', $this->location['today']['time'])?></h4>*/?>
+		<h4><?=$this->config['weekdays'][date('w', $this->location['today']['time'])]?>, <?=date('d.m.Y', $this->location['today']['time'])?></h4>
 	
 		<canvas id="dayChart" class="dayChart" width="640" height="400"></canvas>
 
@@ -23,6 +23,8 @@
 	</div>
 
 	<div id="forecast">
+		<h3>Vorschau</h3>
+
 		<? foreach($this->location['forecast'] as $idx => $day) : ?>
 		<div class="day<?=$idx==0?' active':''?>">
 			<p class="icon"><span class="<?=$detail['icon']?>"></span></p>
@@ -38,9 +40,9 @@
 </div>
 
 <div id="locationBackground" class="type<?=ucfirst($this->location['today']['type']['class'])?>">
-	<? /*
+	<? /* * /?>
 	<img src="http://farm9.staticflickr.com/8278/8710831494_63c5940287_o.jpg" alt="Laufrunde durch Linz">
-	<a href="http://www.flickr.com/photos/friesenecker/8710831494/" title="Laufrunde durch Linz von Daniel Friesenecker bei Flickr">c by</a> */?>
+	<a href="http://www.flickr.com/photos/friesenecker/8710831494/" title="Laufrunde durch Linz von Daniel Friesenecker bei Flickr">c by</a><? /**/?>
 </div>
 
 <? include "_foot.php" ?>
