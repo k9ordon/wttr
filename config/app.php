51934b298f8abcbfe220685cd3acf05ea0c3510e
k9ordon <k9ordon@gmail.com>
2013-07-21 13:38:25 +0200

    added weekdays to config

--- config/app.php
+++ config/app.php
@@ -3,6 +3,13 @@
 $config['base'] = 'http://'.$_SERVER['SERVER_NAME'] . '';
 $config['contentBase'] = $config['base'];
 
+$config['weekdays'] = array(
+	'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samastag', 'Sonntag'
+	);
+$config['weekdaysShort'] = array(
+	'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'
+	);
+
 
 $config['weatherTypes'] = array(
 	//'clear' => array('name' => 'Wolkenlos'), 
