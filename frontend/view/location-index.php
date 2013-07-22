0977dafed08aa6dfcb86533e8e36a842b4e8fd01
k9ordon <k9ordon@gmail.com>
2013-07-22 18:22:30 +0200

    day forcast rename

--- frontend/view/location-index.php
+++ frontend/view/location-index.php
@@ -8,28 +8,34 @@
 		<h3><?=$this->location['today']['type']['name']?></h3>
 		<h4><?=$this->config['weekdays'][date('w', $this->location['today']['time'])]?>, <?=date('d.m.Y H:i', $this->location['today']['time'])?></h4>
 	
-		<div class="dayChart" style="height:100px; width:640px"></div>
+		<div class="dayForeacast">
+			<div class="dayChart" style="height:100px; width:640px"></div>
 
-		<div class="detail">
-			<? foreach($this->location['today']['detail'] as $detail) : ?>
-			<div class="hour">
-				<p class="icon"><span class="<?=$detail['icon']?>"></span></p>
-				<p class="time"><?=date('H:i', $detail['time'])?></p>
-				<p class="temp"><?=$detail['temp']?>°C</p>
-				<p class="rain"><?=$detail['rain']?>%</p>
+			<div class="detail">
+				<? foreach($this->location['today']['detail'] as $detail) : ?>
+				<div class="hour">
+					<p class="icon"><span class="<?=$detail['icon']?>"></span></p>
+					<p class="time"><?=date('H:i', $detail['time'])?></p>
+					<p class="temp"><?=$detail['temp']?>°C</p>
+					<p class="rain"><?=$detail['rain']?>%</p>
+				</div>
+				<? endforeach; ?>
 			</div>
-			<? endforeach; ?>
 		</div>
 	</div>
 
 	<div id="forecast">
 		<h3>Vorschau</h3>
 
+		<div class="forecastChart" style="height:50px; width:640px"></div>
+
 		<? foreach($this->location['forecast'] as $idx => $day) : ?>
 		<div class="day<?=$idx==0?' active':''?>">
 			<p class="icon"><span class="<?=$detail['icon']?>"></span></p>
-			<p class="time">
+			<p class="weekday">
 				<?=$this->config['weekdaysShort'][date('w', $day['time'])]?>
+			</p>
+			<p class="time">
 				<?=date('d.m', $day['time'])?>
 			</p>
 			<p class="temp"><?=$detail['temp']?>°C</p>
@@ -39,7 +45,7 @@
 	</div>
 </div>
 
-<div id="locationBackground" class="type<?=ucfirst($this->location['today']['type']['class'])?>">
+<div id="locationBackground">
 	<? /* * /?>
 	<img src="http://farm9.staticflickr.com/8278/8710831494_63c5940287_o.jpg" alt="Laufrunde durch Linz">
 	<a href="http://www.flickr.com/photos/friesenecker/8710831494/" title="Laufrunde durch Linz von Daniel Friesenecker bei Flickr">c by</a><? /**/?>
