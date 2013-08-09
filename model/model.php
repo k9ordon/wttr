8eb4430716f37250ff8d0e02f2864a0ab6f47c07
Klemens Gordon <github@k94n.com>
2013-08-10 01:50:11 +0200

    added memcache

--- model/model.php
+++ model/model.php
@@ -2,6 +2,14 @@
 
 class model {
 	public function __construct() {}
+
+	// @todo singelton bla
+	protected function getMemcache() {
+		$meminstance = new Memcache();
+		$meminstance->pconnect('localhost', 11211);
+
+		return $meminstance;
+	}
 }
 
 ?>
