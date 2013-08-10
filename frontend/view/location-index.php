f18b70288e077f297bdeecf2c69ff3eb925ef118
Klemens Gordon <github@k94n.com>
2013-08-10 22:05:33 +0200

    added day forecast

--- frontend/view/location-index.php
+++ frontend/view/location-index.php
@@ -1,6 +1,8 @@
 <? include "_head.php" ?>
 
 <div id="backgroundImage">
+
+<? if($this->randomPhoto) : ?>
 <img src="<?=
 sprintf(
 'http://farm6.staticflickr.com/%s/%s_%s_b.jpg',
@@ -8,61 +10,66 @@ $this->randomPhoto['server'],
 $this->randomPhoto['id'],
 $this->randomPhoto['secret'])
 ?>" width="1024" height="673" alt="Linz">
+<? endif; ?>
+
 </div>
 
 <div id="locationWrap">
-	<h1>Wetter <?=$this->location['name']?></h1>
-
-	<div class="dayCard">
-		<h2><?=round($this->weather['main']['temp']); ?><span class="icon-Celsius"></span></h2>
-		<h3><?=$this->weather['weather'][0]['description']?></h3>
-		<? // var_dump($this->weather); ?>
-		<h4><?=$this->config['weekdays'][date('w', $this->weather['dt'])]?>, <?=date('d.m.Y H:i', $this->weather['dt'])?></h4>
+	<h1>Wetter <?var_dump($this->weather)?></h1>
 	
-		<?//=$this->weather['weather'][0]['main']?>
+	<h2><?=round($this->weather['main']['temp']); ?><span class="icon-Celsius"></span></h2>
+	<h3><?=$this->weather['weather'][0]['description']?></h3>
+	<? // var_dump($this->weather); ?>
+	<h4><?=$this->config['weekdays'][date('w', $this->weather['dt'])]?>, <?=date('d.m.Y H:i', $this->weather['dt'])?></h4>
 
-		<div class="hourForeacast">
-			<div class="dayChart" style="height:100px; width:100%"></div>
-			<script>var hourChartData = JSON.parse('<?=$this->hourGraphJson;?>');</script>
+	<?//=$this->weather['weather'][0]['main']?>
 
-			<div class="detail">
-				<? foreach(array_slice($this->hours['list'], 0, 7) as $hour) : ?>
-				<? $hourWeatherType = $this->config['weatherTypes'][$hour['weather'][0]['icon']]; ?>
+	<div class="hourForeacast">
+		<div class="hourChart" style="height:190px; width:100%"></div>
+		<script>var hourChartData = JSON.parse('<?=$this->hourGraphJson;?>');</script>
 
-				<div class="hour">
-				<div class="pin">
-					<?//=$hour['weather'][0]['main']?>
+		<div class="detail">
+			<? foreach(array_slice($this->hours['list'], 0, 7) as $hour) : ?>
+			<? $hourWeatherType = $this->config['weatherTypes'][$hour['weather'][0]['icon']]; ?>
 
-					<p class="icon <?=$hourWeatherType['class']?>"><span class="<?=$hourWeatherType['icon']?>"></span></p>
-					<p class="time"><?=date('D H:i', $hour['dt'])?></p>
-					<p class="temp"><?=$hour['main']['temp']?>°C</p>
-					<!--<p class="rain"><?var_dump($hour['rain'])?></p>-->
-				</div>
-				</div>
-				<? endforeach; ?>
+			<div class="hour">
+			<div class="pin">
+				<?//=$hour['weather'][0]['main']?>
+
+				<p class="icon <?=$hourWeatherType['class']?>"><span class="<?=$hourWeatherType['icon']?>"></span></p>
+				<p class="time"><?=$this->config['weekdaysShort'][date('w', $hour['dt'])]?> <?=date('H:i', $hour['dt'])?></p>
+				<p class="temp"><?=$hour['main']['temp']?>°C</p>
+				<!--<p class="rain"><?var_dump($hour['rain'])?></p>-->
 			</div>
+			</div>
+			<? endforeach; ?>
 		</div>
 	</div>
 
-	<div id="forecast">
-		<h3>Vorschau</h3>
+	<? /*<h3>Vorschau</h3> */ ?>
 
+	<div class="dayForecast">
 		<div class="forecastChart" style="height:50px; width:640px"></div>
+		<div class="detail">
 
-		<? foreach($this->location['forecast'] as $idx => $day) : ?>
-		<div class="day<?=$idx==0?' active':''?>">
-			<p class="icon"><span class="<?=$detail['icon']?>"></span></p>
-			<p class="weekday">
-				<?=$this->config['weekdaysShort'][date('w', $day['time'])]?>
-			</p>
-			<p class="time">
-				<?=date('d.m', $day['time'])?>
-			</p>
-			<p class="temp"><?=$detail['temp']?>°C</p>
-			<p class="rain"><?=$detail['rain']?>%</p>
+			<? foreach(array_slice($this->days['list'], 0, 14) as $idx => $day) : ?>
+			<? $dayWeatherType = $this->config['weatherTypes'][$day['weather'][0]['icon']]; ?>
+
+			<div class="day">
+			<div class="pin">
+				<?//=$hour['weather'][0]['main']?>
+
+				<p class="icon <?=$dayWeatherType['class']?>"><span class="<?=$dayWeatherType['icon']?>"></span></p>
+				<p class="time"><?=$this->config['weekdaysShort'][date('w', $day['dt'])]?></p>
+				<!--<p class="temp"><?//=$day['main']['temp']?>°C</p>
+				<p class="rain"><?var_dump($day['rain'])?></p>-->
+			</div>
+			</div>
+			<? endforeach; ?>
+
 		</div>
-		<? endforeach; ?>
 	</div>
+
 </div>
 
 <div id="ad">
