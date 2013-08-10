f40545534278e587a9c2137676f6783af0ccaf5f
Klemens Gordon <github@k94n.com>
2013-08-10 02:32:51 +0200

    chaned to dev url

--- frontend/view/_nav.php
+++ frontend/view/_nav.php
@@ -1,4 +1,4 @@
 <nav id="main-nav">
-	<span><a id="logo" href="#">wttr.at</a></span>
+	<span><a id="logo" href="#">wttr.k94n.com</a></span>
 	<span id="location" href="#">/<?=$this->location['name']?></span>
 </nav>
