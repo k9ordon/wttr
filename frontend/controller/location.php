695b9ef795aa85f2c2b1029648fd3d0003e19418
Klemens Gordon <github@k94n.com>
2013-08-10 13:53:53 +0200

    implerement place

--- frontend/controller/location.php
+++ frontend/controller/location.php
@@ -2,9 +2,15 @@
 
 class Controller_Location extends Controller {
 
+	protected $locationQuery = 'Linz AT';
+
 	public function Action_Index() {
 		if(array_key_exists('local', $_GET) && $_GET['local'] == true) {
 			$this->htmlClassList[] = 'local';
+		}
+
+		if(array_key_exists(1, $this->router->urlParts) && $this->router->urlParts[1]) {
+			$this->locationQuery = $this->router->urlParts[1];
 		}
 
 		$this->getLocationData();
@@ -15,16 +21,12 @@ class Controller_Location extends Controller {
 
 		$this->getLocationPhotos();
 		$this->getHourGraphJson();
-
-		if(array_key_exists(2, $this->router->urlParts) && $this->router->urlParts[2]) {
-			$this->location['name'] = $this->router->urlParts[2];
-		}
 		
 		$this->render('location-index');
 	}
 
 	protected function getLocationData() {
-		$Model_Owm = new Model_Owm(2772400);
+		$Model_Owm = new Model_Owm($this->locationQuery);
 		$this->location = $this->config['fake']['locationData'];
 		
 		$this->weather = $Model_Owm->getWeather();
@@ -34,18 +36,19 @@ class Controller_Location extends Controller {
 	}
 
 	protected function getLocationPhotos() {
-		$photoApi = new Model_Flickrphotos();
+		$photoApi = new Model_Flickrphotos($this->locationQuery);
 
 		// day
-		$result = $photoApi->search($this->weatherType['flickrtag'], 'xum7xdNQUL_5UeTriA');
+		$result = $photoApi->search($this->weatherType['flickrtag']);
 		
 		// night
 		//$result = $photoApi->search('Nacht', 'xum7xdNQUL_5UeTriA');
 
 		$this->locationPhotos = $result['photos']['photo'];
-		$threshold = 10;
 		$count = count($this->locationPhotos);
-		if($threshold < round($count/10)) $threshold = 4;
+
+		if(round($count/10) < 1) $threshold = 3;
+		else $threshold = round($count/10);
 
 		$this->randomPhoto = $this->locationPhotos[rand(0,$threshold)];
 
@@ -60,9 +63,9 @@ class Controller_Location extends Controller {
 
 		);
 		foreach(array_slice($this->hours['list'], 0, 7) as $idx => $hour) {
-			array_push($array['temp'], array($idx, $hour['main']['temp']));
-			array_push($array['rain'], array($idx, $hour['rain']['3h']));
-			array_push($array['clouds'], array($idx, $hour['clouds']['all']));
+			@$hour['main']['temp'] ? array_push($array['temp'], array($idx, $hour['main']['temp'])) : false;
+			@$hour['rain']['3h'] ? array_push($array['rain'], array($idx, $hour['rain']['3h'])) : false;
+			@$hour['clouds']['all'] ? array_push($array['clouds'], array($idx, $hour['clouds']['all'])) : false;
 		}
 
 		$this->hourGraphJson = json_encode($array);
