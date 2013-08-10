c53af2d20f7b6484e4ca323884787e43360ccaa9
Klemens Gordon <github@k94n.com>
2013-08-10 22:06:30 +0200

    weather station to nav & fixed title

--- frontend/view/_head.php
+++ frontend/view/_head.php
@@ -1,6 +1,6 @@
 <html id="<?=strtolower(substr(get_class($this),11))?>" class="<?=implode($this->htmlClassList, ' ')?>">
 <head>
-<title>wttr</title>
+<title>wttr.k94n.com</title>
 <? foreach(array($this->config['base']."/assets/built/less/app.css") as $s) : ?>
 	<link href="<?=$s?>" rel="stylesheet" type="text/css" media="screen" />
 <? endforeach; ?>
--- frontend/view/_nav.php
+++ frontend/view/_nav.php
@@ -1,4 +1,4 @@
 <nav id="main-nav">
 	<span><a id="logo" href="#">wttr.k94n.com</a></span>
-	<span id="location" href="#">/<?=$this->location['name']?></span>
+	<span id="location" href="#">/<?=$this->weather['name']?>, <?=$this->weather['sys']['country']?></span>
 </nav>
