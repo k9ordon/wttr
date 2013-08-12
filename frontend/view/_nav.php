4988cd8d01235474b0572171100813af0de44d42
Klemens Gordon <github@k94n.com>
2013-08-12 21:16:39 +0200

    there is no beta

--- frontend/view/_nav.php
+++ frontend/view/_nav.php
@@ -1,6 +1,6 @@
 <nav id="main-nav">
 	<span><a id="logo" href="http://wttr.k94n.com">wttr.k94n.com</a></span>
-	<span id="preview"><a target="_blank" href="https://github.com/k9ordon/wttr/issues">Beta Version</a></span>
+	<span id="preview"><a target="_blank" href="https://github.com/k9ordon/wttr/issues">Public Github Checkout</a></span>
 
 	<span id="location" href="#">- <?=$this->weather['name']?> <?=$this->weather['sys']['country']?></span>
 </nav>
