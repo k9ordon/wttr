32d102e78f59ed42b1f334b779a37f9ac6d1d294
Klemens Gordon <github@k94n.com>
2013-08-13 22:21:41 +0200

    changed nav link

--- frontend/view/_nav.php
+++ frontend/view/_nav.php
@@ -1,6 +1,6 @@
 <nav id="main-nav">
 	<span><a id="logo" href="http://wttr.at">wttr.at</a></span>
-	<span id="preview"><a target="_blank" href="https://github.com/k9ordon/wttr/issues">Public Github Checkout</a></span>
+	<span id="preview"><a target="_blank" href="https://wttr.at">Development Version - Read More</a></span>
 
 	<span id="location" href="#">- <?=$this->weather['name']?> <?=$this->weather['sys']['country']?></span>
 </nav>
